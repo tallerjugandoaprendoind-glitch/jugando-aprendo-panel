465e01cea0a9de40a3a9a8a20e6158cc3ffc207b
tallerjugandoaprendoind-glitch <tallerjugandoaprendoind@gmail.com>
2026-02-18 16:07:05 -0500

    cambios grandasosososo

--- proyecto_fixed/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-// UBICACIÓN: lib/supabase.ts
-import { createBrowserClient } from '@supabase/ssr'
-
-// Función para crear el cliente en el navegador
-// Usamos createBrowserClient para que maneje las cookies automáticamente
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
